5672a6ee97c0bc7a496f4357e248fa9d2d98f7fb
Kevin Workman <kevinaworkman@gmail.com>
2019-11-17 19:34:50 -0800

    reset back to before walkthrough

--- week-2-server/project-1-comments/src/main/java/io/happycoding/servlets/CommentsServlet.java
+++ week-2-server/project-1-comments/src/main/java/io/happycoding/servlets/CommentsServlet.java
@@ -1,8 +1,5 @@
 package io.happycoding.servlets;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,39 +9,9 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/comments")
 public class CommentsServlet extends HttpServlet {
 
-  private List<String> comments = new ArrayList<>();
-
-  @Override
-  public void init(){
-    comments.add("Hello!");
-    comments.add("Hey.");
-    comments.add("Hi");
-  }
-
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("application/json;");
-
-    response.getWriter().println("[");
-    for(int i = 0; i < comments.size(); i++) {
-      String comment = comments.get(i);
-      response.getWriter().println("\"" + comment + "\"");
-
-      // don't print a comma after the last comment
-      if(i < comments.size() - 1){
-        response.getWriter().println(",");
-      }
-    }
-    response.getWriter().println("]");
+    response.setContentType("text/html;");
+    response.getOutputStream().println("<h1>Hello world!</h1>");
   }
-
-@Override
-public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-  String message = request.getParameter("message");
-  comments.add(message);
-  response.sendRedirect("/index.html");
-}
-
-
- 
 }
