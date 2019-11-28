0ede66dfbdbda9679a049948cfe1ab74484b8985
Kevin Workman <kevinaworkman@gmail.com>
2019-11-27 19:19:12 -0800

    goodbye world

--- hello-world/src/main/java/io/happycoding/gcloud/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package io.happycoding.gcloud;
-
-public class HelloWorld {
-  public static void main(String... args){
-    Greeter greeter = new Greeter("Hello");
-    String greetingMessage = greeter.getGreetingMessage("world");
-    System.out.println(greetingMessage);
-  }
-}
